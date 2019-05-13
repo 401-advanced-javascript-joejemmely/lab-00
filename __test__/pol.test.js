fe4e265666db42def54715721ec92463c3440292
Joé Jemmely <joe.jemmely@gmail.com>
2019-05-13 15:17:49 -0700

    Make the test failed

--- __test__/pol.test.js
+++ __test__/pol.test.js
@@ -3,13 +3,11 @@
 let pol = require('../pol.js');
 
 describe('proof of life', () => {
-
   it('lives by default', () => {
-    expect(pol.isAlive()).toBeTruthy();
+    expect(pol.isAlive('d')).toBeTruthy();
   });
 
   it('dies with a param', () => {
     expect(pol.isAlive('x')).toBeFalsy();
   });
-
 });
